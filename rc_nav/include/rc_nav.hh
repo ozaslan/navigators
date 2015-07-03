c961632548b2d0419ab3f084403f0a7fa227b82f
Salih Tolga Ozaslan <ozaslan@seas.upenn.edu>
2015-07-03 19:39:41 -0400

    -- Adding launch files for rc_nav and updating other files.

--- rc_nav/include/rc_nav.hh
+++ rc_nav/include/rc_nav.hh
@@ -7,54 +7,28 @@
 
 #include <ros/ros.h>
 
-#include <utilities.hh>
+#include <utils.hh>
+#include <rc_proc.hh>
 #include <com_msgs/RC.h>
 #include <cont_msgs/Heading.h>
 
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <nav_msgs/Path.h>
 #include <nav_msgs/Odometry.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Imu.h>
 
 using namespace std;
 
-ros::Time curr_time;
-ros::Time prev_time;
-double dt;
-
-// This is for 2D and NULL maps
-double max_elevation, min_elevation;
-// Radius of the spherical/circular robot approximation
-double safety_margin;
 // Max norm of the velocity vector and yawing speed
 double max_lin_speed,
 	   max_rot_speed;
-// Margin around the middle of sticks inside which
-// zero command is applied
-double	dead_zone_x,
-		dead_zone_y,
-		dead_zone_z,
-		dead_zone_psi;
-// RC device specific values
-double	min_rc_value,
-		max_rc_value;
-
 // Flags for the node
 double refresh_rate;
 bool   debug_mode;
 
-int process_inputs(const ros::NodeHandle &n);
-int setup_messaging_interface(ros::NodeHandle &n);
-int generate_heading();
-// Pose estimate (This is treated as GT by the navigator)
+int  process_inputs(const ros::NodeHandle &n);
+int  setup_messaging_interface(ros::NodeHandle &n);
+int  loop(const ros::NodeHandle &n);
 void odom_callback(const nav_msgs::Odometry &msg);
-// RC commands
+void imu_callback(const sensor_msgs::Imu &msg);
 void rc_callback(const com_msgs::RC &msg);
-void pcl_callback(const sensor_msgs::PointCloud2 &msg);
-void gridmap_callback(const nav_msgs::OccupancyGrid &msg);
-int loop(const ros::NodeHandle &n);
-
 
 #endif
--- rc_nav/src/rc_nav_node.cc
+++ rc_nav/src/rc_nav_node.cc
@@ -1,239 +1,192 @@
 #include "rc_nav.hh"
 
-#define Dt(t2, t1) (((double)t2.sec  - t1.sec ) + \
-					((double)t2.nsec - t1.nsec) / 1e9);
-
-
 ros::Subscriber odom_subs;
-ros::Subscriber gridmap_subs;
-ros::Subscriber pcl_subs;
+ros::Subscriber imu_subs;
 ros::Subscriber rc_subs;
 ros::Publisher  heading_publ;
-// The below two are for visualization purposes only
-// Has no effect on the flow of the algorithm
-ros::Publisher  path_publ;
-ros::Publisher  goal_publ; // odom message
 
-nav_msgs::Odometry		 odom_msg;
-nav_msgs::OccupancyGrid  gridmap_msg;
-sensor_msgs::PointCloud2 pcl_msg;
-cont_msgs::Heading		 heading_msg;
-com_msgs::RC			 rc_msg;
-
-bool got_odom_msg = false;
+nav_msgs::Odometry odom_msg;
+sensor_msgs::Imu imu_msg;
+RCProc rc_proc;
 
 /*
--- loop
-
--- inside the RC_CMD callback
-	-- Check if the hypotetical sphere/circle is touching the 2D/3D map
-	-- Update 'to' paramteres of the cont_msgs
-	-- publish
+  - process the inputs
+  - setup messaging interface
+  - setup callbacks
+  - loop
 */
-
-int loop(const ros::NodeHandle &n);
 
 int main(int argc, char* argv[]){
 
-	ros::init(argc, argv, "carters_estimator");
-	ros::NodeHandle n("~");
+  ros::init(argc, argv, "rc_nav_node");
+  ros::NodeHandle n("~");
 
-	process_inputs(n);
+  process_inputs(n);
 
-	setup_messaging_interface(n);
-        
-	loop(n);        
-        
-    return 1;
+  setup_messaging_interface(n);
+
+  loop(n);        
+
+  return 1;
 }
 
 int process_inputs(const ros::NodeHandle &n)
 {
-	n.param("safety_margin", safety_margin, 1.0);
-	n.param("min_elevation", min_elevation, 0.0);
-	n.param("max_elevation", max_elevation, 1.0);
-	n.param("max_lin_speed", max_lin_speed, 1.0);
-	n.param("max_rot_speed", max_rot_speed, 0.50);
-	n.param("min_rc_value", min_rc_value,    0.0);
-	n.param("max_rc_value", max_rc_value, 1000.0);
-	
-	n.param("refresh_rate"    , refresh_rate    , 300.0);
-	n.param("debug_mode"      , debug_mode      , false);
+  string rc_config_path;
 
-	n.param("dead_zone_x"   , dead_zone_x  ,  15.0); // forward
-	n.param("dead_zone_y"   , dead_zone_y  ,  15.0); // lateral
-	n.param("dead_zone_z"   , dead_zone_z  ,  20.0); // vertical
-	n.param("dead_zone_psi" , dead_zone_psi,  15.0);
+  n.param("max_lin_speed", max_lin_speed, 1.0);
+  n.param("max_rot_speed", max_rot_speed, DEG2RAD(23));
 
-	ROS_INFO(" ---------------- RC NAVIGATOR ------------------");
-	ROS_INFO("[debug_mode] ----------- : [%s]", debug_mode ? "TRUE" : "FALSE");
-	ROS_INFO("[refresh_rate] --------- : [%.3lf]", refresh_rate);
-	ROS_INFO("[safety_margin] -------- : [%.3lf]", safety_margin);
-	ROS_INFO("[min, max]_elevation --- : [%.3lf, %.3lf]", min_elevation, max_elevation);
-	ROS_INFO("max_[lin, rot]_speed --- : [%.3lf, %.3lf]", max_lin_speed , max_rot_speed);
-	ROS_INFO("[min, max]_rc_value ---- : [%.3lf, %.3lf]", min_rc_value , max_rc_value);
-	ROS_INFO("dead_zone_[x, y, z, psi] : [%.3lf, %.3lf, %.3lf, %.3lf]", 
-														  dead_zone_x, 
-														  dead_zone_y,
-														  dead_zone_z,
-														  dead_zone_psi);
-	ROS_INFO(" ------------------------------------------------");
-	return 0;
+  n.param("refresh_rate"    , refresh_rate    , 300.0);
+  n.param("debug_mode"      , debug_mode      , false);
+  n.param("rc_config_path"  , rc_config_path  , string(""));
+
+  // RC Related params
+  rc_proc.load_params(rc_config_path);
+  rc_proc.normalize() = true;
+
+  ROS_INFO(" ---------------- RC NAVIGATOR ------------------");
+  ROS_INFO("[debug_mode] ----------- : [%s]", debug_mode ? "TRUE" : "FALSE");
+  ROS_INFO("[refresh_rate] --------- : [%.3lf]", refresh_rate);
+  ROS_INFO("[rc_config_path] ------- : [%s]" , rc_config_path.c_str());
+  ROS_INFO("max_[lin, rot]_speed --- : [%.3lf, %.3lf]", max_lin_speed , max_rot_speed);
+  rc_proc.print_params();
+  ROS_INFO(" ------------------------------------------------");
+
+  odom_msg.pose.pose.position.x = 0;
+  odom_msg.pose.pose.position.y = 0;
+  odom_msg.pose.pose.position.z = 0;
+  odom_msg.pose.pose.orientation.w = 1;
+  odom_msg.pose.pose.orientation.x = 0;
+  odom_msg.pose.pose.orientation.y = 0;
+  odom_msg.pose.pose.orientation.z = 0;
+  odom_msg.twist.twist.linear.x = 0;
+  odom_msg.twist.twist.linear.y = 0;
+  odom_msg.twist.twist.linear.z = 0;
+  odom_msg.twist.twist.angular.x = 0;
+  odom_msg.twist.twist.angular.y = 0;
+  odom_msg.twist.twist.angular.z = 0;
+
+  return 0;
 }
 
 int setup_messaging_interface(ros::NodeHandle &n)
 {
-	odom_subs    = n.subscribe("odom" , 10,    odom_callback, ros::TransportHints().tcpNoDelay());
-	rc_subs 	 = n.subscribe("rc"   , 10,		 rc_callback, ros::TransportHints().tcpNoDelay());
-	pcl_subs 	 = n.subscribe("map3D", 10,	    pcl_callback, ros::TransportHints().tcpNoDelay());
-	gridmap_subs = n.subscribe("map2D", 10, gridmap_callback, ros::TransportHints().tcpNoDelay());
-	heading_publ = n.advertise<cont_msgs::Heading>("heading", 10);
-	path_publ	 = n.advertise<nav_msgs::Path    >("path"   , 10);
-	goal_publ	 = n.advertise<nav_msgs::Odometry>("goal"   , 10);
+  imu_subs      = n.subscribe("imu"  , 10,  imu_callback, ros::TransportHints().tcpNoDelay());
+  odom_subs     = n.subscribe("odom" , 10, odom_callback, ros::TransportHints().tcpNoDelay());
+  rc_subs       = n.subscribe("rc"   , 10,   rc_callback, ros::TransportHints().tcpNoDelay());
+  heading_publ  = n.advertise<cont_msgs::Heading>("heading", 10);
 
-	return 0;
-}
-
-void odom_callback(const nav_msgs::Odometry &msg){
-	got_odom_msg = true;
-	odom_msg = msg;
-	if(debug_mode)
-		ROS_INFO("RC NAV : Got ODOM message!");
-}
-
-void rc_callback(const com_msgs::RC &msg){
-	// -- generate the 'heading_msg'
-	// -- ### check if it is inside the map / there is any collision
-	// -- generate the 'path_msg'
-	// -- generate the 'goal_msg'
-	// -- publish all above
-
-	//###
-	//if(got_odom_msg == false)
-	//	return;
-	odom_msg.pose.pose.orientation.w = 1;
-
-	rc_msg = msg;
-
-	prev_time = curr_time;
-	curr_time = ros::Time::now();
-
-	static int rc_vals_span = (max_rc_value - min_rc_value) / 2;
-	static int mid_rc_value = (max_rc_value + min_rc_value) / 2;
-	// Calculate the velocities
-	int x_stick_pos   = rc_msg.right_fb - mid_rc_value;
-	int y_stick_pos   = rc_msg.right_rl - mid_rc_value;
-	int z_stick_pos   =  rc_msg.left_fb - mid_rc_value;
-	int psi_stick_pos =  rc_msg.left_rl - mid_rc_value;
-
-	//cout << x_stick_pos << " " << y_stick_pos << " " << z_stick_pos << " " << psi_stick_pos << endl;
-
-	// ---
-	if(x_stick_pos > dead_zone_x)
-		x_stick_pos -= dead_zone_x;
-	else if(x_stick_pos < -dead_zone_x)
-		x_stick_pos += dead_zone_x;
-	else
-		x_stick_pos = 0;
-	// ---
-	if(y_stick_pos > dead_zone_y)
-		y_stick_pos -= dead_zone_y;
-	else if(y_stick_pos < -dead_zone_y)
-		y_stick_pos += dead_zone_y;
-	else
-		y_stick_pos = 0;
-	// ---
-	if(z_stick_pos > dead_zone_z)
-		z_stick_pos -= dead_zone_z;
-	else if(z_stick_pos < -dead_zone_z)
-		z_stick_pos += dead_zone_z;
-	else
-		z_stick_pos = 0;
-	// ---
-	if(psi_stick_pos > dead_zone_psi)
-		psi_stick_pos -= dead_zone_psi;
-	else if(psi_stick_pos < -dead_zone_psi)
-		psi_stick_pos += dead_zone_psi;
-	else
-		psi_stick_pos = 0;
-
-	double x_vel   = -(double)x_stick_pos   / (rc_vals_span - dead_zone_x) * max_lin_speed;
-	double y_vel   =  (double)y_stick_pos   / (rc_vals_span - dead_zone_y) * max_lin_speed;
-	double z_vel   = -(double)z_stick_pos   / (rc_vals_span - dead_zone_z) * max_lin_speed;
-	double psi_vel =  (double)psi_stick_pos / (rc_vals_span - dead_zone_x) * max_rot_speed;
-
-	dt = Dt(curr_time, prev_time);
-
-	heading_msg.header.stamp = curr_time;
-	heading_msg.header.frame_id = "world";
-	heading_msg.header.seq++;
-	
-	heading_msg.pos_from = odom_msg.pose.pose.position;
-	heading_msg.vel_from = odom_msg.twist.twist.linear;
-	heading_msg.acc_from.x = 
-		heading_msg.acc_from.y = 
-		heading_msg.acc_from.z = 0;
-	heading_msg.quat_from  = odom_msg.pose.pose.orientation;
-	heading_msg.omega_from = odom_msg.twist.twist.angular;
-	heading_msg.domega_from.x = 
-		heading_msg.domega_from.y = 
-		heading_msg.domega_from.z = 0;
-	
-	heading_msg.pos_to.x = heading_msg.pos_from.x + x_vel * dt;
-	heading_msg.pos_to.y = heading_msg.pos_from.y + y_vel * dt;
-	heading_msg.pos_to.z = heading_msg.pos_from.z + z_vel * dt;
-	heading_msg.vel_to.x = x_vel;
-	heading_msg.vel_to.y = y_vel;
-	heading_msg.vel_to.z = z_vel;
-	heading_msg.acc_to = heading_msg.acc_from;
-	// quat comes here
-	Vector4d quat;
-	quat(0) = heading_msg.quat_from.w;
-	quat(1) = heading_msg.quat_from.x;
-	quat(2) = heading_msg.quat_from.y;
-	quat(3) = heading_msg.quat_from.z;
-	
-	Vector3d rpy = quat2rpy(quat);
-	rpy(2) += dt * psi_vel;
-	quat = rpy2quat(rpy);
-	heading_msg.quat_to.w = quat(0);
-	heading_msg.quat_to.x = quat(1);
-	heading_msg.quat_to.y = quat(2);
-	heading_msg.quat_to.z = quat(3);
-	//
-	heading_msg.omega_to.x = 
-		heading_msg.omega_to.y = 0; 
-	heading_msg.omega_to.z = psi_vel;
-	heading_msg.domega_to = heading_msg.domega_from;
-
-	heading_publ.publish(heading_msg);
-	
-	if(debug_mode)
-		ROS_INFO("RC NAV : Got RC message!");
-}
-
-void pcl_callback(const sensor_msgs::PointCloud2 &msg){
-	pcl_msg = msg;
-	if(debug_mode)
-		ROS_INFO("RC NAV : Got PCL message!");
-}
-
-void gridmap_callback(const nav_msgs::OccupancyGrid &msg){
-	gridmap_msg = msg;
-	if(debug_mode)
-		ROS_INFO("RC NAV : Got GRIDMAP message!");
+  return 0;
 }
 
 int loop(const ros::NodeHandle &n)
 {
-	ros::Rate r(refresh_rate); 
+  ros::Rate r(refresh_rate); 
 
-	prev_time = ros::Time::now();
-	curr_time = ros::Time::now();
+  ros::spin();
 
-	ros::spin();
-
-	return 0;
+  return 0;
 }
 
-#undef Dt
+void imu_callback(const sensor_msgs::Imu &msg){
+  imu_msg = msg;
+  if(debug_mode)
+    ROS_INFO("RC NAV : Got IMU message!");
+}
+
+void odom_callback(const nav_msgs::Odometry &msg){
+  odom_msg = msg;
+  if(debug_mode)
+    ROS_INFO("RC NAV : Got ODOM message!");
+}
+
+void rc_callback(const com_msgs::RC &msg){
+  double rc_x, rc_y, rc_z, rc_psi;
+ 
+  rc_proc.process(msg, rc_x, rc_y, rc_z, rc_psi);
+
+  /*
+  cout << "rc_x   = " << rc_x   << endl;
+  cout << "rc_y   = " << rc_y   << endl;
+  cout << "rc_z   = " << rc_z   << endl;
+  cout << "rc_psi = " << rc_psi << endl;
+  */
+
+  double x_vel   = rc_x;
+  double y_vel   = rc_y;
+  double z_vel   = rc_z;
+  double psi_vel = rc_psi * max_rot_speed;
+
+  double vel_norm = sqrt(rc_x * rc_x + rc_y * rc_y + rc_z * rc_z) + 1e-6;
+
+  if(vel_norm > max_lin_speed){
+    x_vel *= max_lin_speed / vel_norm;
+    y_vel *= max_lin_speed / vel_norm;
+    z_vel *= max_lin_speed / vel_norm;
+  }
+
+  static ros::Time prev_time = ros::Time::now();
+  ros::Time curr_time = ros::Time::now();
+  
+  double dt = (curr_time - prev_time).toSec();
+  prev_time = curr_time;
+  static cont_msgs::Heading heading_msg;
+
+  heading_msg.header.stamp = curr_time;
+  heading_msg.header.frame_id = "world";
+  heading_msg.header.seq++;
+
+  heading_msg.pos_from = odom_msg.pose.pose.position;
+  heading_msg.vel_from = odom_msg.twist.twist.linear;
+  heading_msg.acc_from.x = 
+    heading_msg.acc_from.y = 
+    heading_msg.acc_from.z = 0;
+  heading_msg.quat_from  = imu_msg.orientation;
+  heading_msg.omega_from = odom_msg.twist.twist.angular;
+  heading_msg.domega_from.x = 
+    heading_msg.domega_from.y = 
+    heading_msg.domega_from.z = 0;
+
+  heading_msg.pos_to = heading_msg.pos_from;
+  //heading_msg.pos_to.x = heading_msg.pos_from.x;
+  //heading_msg.pos_to.y = heading_msg.pos_from.y;
+  //heading_msg.pos_to.z = heading_msg.pos_from.z;
+  heading_msg.vel_to.x = x_vel;
+  heading_msg.vel_to.y = y_vel;
+  heading_msg.vel_to.z = z_vel;
+  heading_msg.acc_to = heading_msg.acc_from;
+
+  // quat comes here
+  Vector4d quat;
+  quat(0) = heading_msg.quat_from.w;
+  quat(1) = heading_msg.quat_from.x;
+  quat(2) = heading_msg.quat_from.y;
+  quat(3) = heading_msg.quat_from.z;
+
+  Vector3d rpy = utils::trans::quat2rpy(quat);
+  rpy(2) += dt * psi_vel;
+
+	rpy(2) = 0; //###
+
+  quat = utils::trans::rpy2quat(rpy);
+  heading_msg.quat_to.w = quat(0);
+  heading_msg.quat_to.x = quat(1);
+  heading_msg.quat_to.y = quat(2);
+  heading_msg.quat_to.z = quat(3);
+
+	heading_msg.quat_from = heading_msg.quat_to; //###
+  //
+  heading_msg.omega_to.x = 
+    heading_msg.omega_to.y = 0; 
+  heading_msg.omega_to.z = psi_vel;
+  heading_msg.domega_to = heading_msg.domega_from;
+
+  heading_publ.publish(heading_msg);
+
+  if(debug_mode)
+    ROS_INFO("RC NAV : Got RC message!");
+}
+
+
+
